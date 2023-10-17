fc93bed4340f3ec3126dad4c5219453c1c650d7b
Dcruz05 <dcruz11@ucol.mx>
2023-10-17 17:24:08 -0600

    Implementacion de alert con text input

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
-import { Button, Switch, StyleSheet,TextInput, Text, View } from 'react-native';
+import { Button, Switch, StyleSheet,TextInput,Alert, Text, View } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
@@ -10,6 +10,12 @@ export default class App extends Component {
       inputTextValue:""
     }
   }
+  alertToInputText = () =>
+  Alert.alert(`${this.state.inputTextValue}`, 'Es lo que escribio', [
+    {text: 'OK', onPress: () => console.log('OK Pressed')},
+  ]);
+
+
   onChange = (value) => {
     console.warn(`El switch cambiara a ${value}`)
     this.setState({switchValue: value})
@@ -27,14 +33,14 @@ export default class App extends Component {
           style={styles.input}
           onChangeText={value => this.onChangeText(value)}
           value={this.state.inputTextValue}
-          placeholder="useless placeholder"
+          placeholder="Ingresa algo"
         />
         <Switch
           onValueChange={()=>{this.onChange(!this.state.switchValue)}}
           value={this.state.switchValue}
         />
         <Button
-          onPress={this.onPressLearnMore}
+          onPress={this.alertToInputText}
           title="Learn More"
           color="#841584"
           accessibilityLabel="Learn more about this purple button"
