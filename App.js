8cdab1c2a8944bf53f1d6a91d0d2f302a734cdaf
JohnyDeCoder <johnymora251@gmail.com>
2023-10-24 08:16:05 -0600

    Added react-native-router-flux package

--- App.js
+++ App.js
@@ -1,67 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Component } from 'react';
-import { Button, Switch, StyleSheet,TextInput,Alert, Text, View } from 'react-native';
+import React, { Component } from "react";
+import LoginView from "./src/LoginView";
+import { Actions, Router, Scene } from "react-native-router-flux";
+
+const scenes = Actions.create(
+  <Scene key="root">
+    <Scene key="login" component={LoginView} title="Login" hideNavBar />
+  </Scene>
+);
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      switchValue: false,
-      inputTextValue:""
-    }
-  }
-  alertToInputText = () =>
-  Alert.alert(`${this.state.inputTextValue}`, 'Es lo que escribio', [
-    {text: 'OK', onPress: () => console.log('OK Pressed')},
-  ]);
-
-
-  onChange = (value) => {
-    console.warn(`El switch cambiara a ${value}`)
-    this.setState({switchValue: value})
-  }
-  onChangeText = (value)=>{
-    this.setState({inputTextValue: value})
-  }
-  onPressLearnMore = () => {
-    console.warn(`Escribiste: ${this.state.inputTextValue}`)
-  }
   render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          onChangeText={value => this.onChangeText(value)}
-          value={this.state.inputTextValue}
-          placeholder="Ingresa algo"
-        />
-        <Switch
-          onValueChange={()=>{this.onChange(!this.state.switchValue)}}
-          value={this.state.switchValue}
-        />
-        <Button
-          onPress={this.alertToInputText}
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <StatusBar style="auto" />
-      </View>
-    );
+    return <Router scenes={scenes} />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },  
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
---
+++ src/LoginView.js
@@ -0,0 +1,73 @@
+import { StatusBar } from "expo-status-bar";
+import { Component } from "react";
+import {
+  Button,
+  Switch,
+  StyleSheet,
+  TextInput,
+  Alert,
+  View,
+} from "react-native";
+
+export default class LoginView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchValue: false,
+      inputTextValue: "",
+    };
+  }
+  alertToInputText = () =>
+    Alert.alert(`${this.state.inputTextValue}`, "Es lo que escribio", [
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+
+  onChange = (value) => {
+    console.warn(`El switch cambiara a ${value}`);
+    this.setState({ switchValue: value });
+  };
+  onChangeText = (value) => {
+    this.setState({ inputTextValue: value });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(value) => this.onChangeText(value)}
+          value={this.state.inputTextValue}
+          placeholder="Ingresa algo"
+        />
+        <Switch
+          onValueChange={() => {
+            this.onChange(!this.state.switchValue);
+          }}
+          value={this.state.switchValue}
+        />
+        <Button
+          onPress={this.alertToInputText}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
